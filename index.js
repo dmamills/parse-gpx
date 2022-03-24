ce58acc0d8badc94d82f608acb6535c7892f1110
dmills <d.mills@vehikl.com>
2022-03-23 23:07:54 -0400

    wip

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 "use strict";
 
 const xml2js = require('xml2js');
-const fs = require('fs');
 const parseTrack = require('./src/parseTrack');
 
-module.exports = (filename) => {
-
-    return new Promise((res, rej) => {
-        fs.readFile(filename,'utf8', (err, data) => {
-            if(err) {
-                rej(err);
-                return;
-            }
-
-            let parser = new xml2js.Parser();
-            parser.parseString(data, (err, xml) => {
-                if(err) {
-                    rej(err);
-                } else {
-                    res(parseTrack(xml.gpx.trk));
-                }
-            });
-        });
-    });
-};
+module.exports = async (xmlStr) => {
+  const xml = await xml2js.parseStringPromise(xmlStr);
+  return parseTrack(xml.gpx.trk);
+}
--- tests/test.js
+++ tests/test.js
@@ -9,97 +9,71 @@ const fs = require('fs');
 const xml2js = require('xml2js');
 
 describe('TrackPoint', () => {
-    it('should be constructable!', () => {
-        let tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z");
-
-        tp.should.have.property('latitude');
-        tp.should.have.property('longitude');
-        tp.should.have.property('timestamp');
-        tp.should.have.property('elevation');
-        tp.should.have.property('heartrate');
-        tp.should.have.property('cadence');
-
-        tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z", 160);
-        tp.heartrate.should.equal(160);
-
-        tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z", null, 180);
-        tp.cadence.should.equal(180);
-
+  it('should be constructable!', () => {
+    let tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z");
+    const expectedProperties = ['latitude', 'longitude', 'timestamp', 'elevation', 'heartrate', 'cadence'];
+    expectedProperties.forEach(prop => {
+      tp.should.have.property(prop);
     });
+
+    tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z", 160);
+    tp.heartrate.should.equal(160);
+
+    tp = new TrackPoint(1,2,3,"2016-06-09T15:59:45.000Z", null, 180);
+    tp.cadence.should.equal(180);
+  });
 })
 
-describe('parseTrack', () => {
-    it('should parse gpx track', () => {
-        fs.readFile(__dirname + '/test.gpx','utf8', (err, data) => {
-            let parser = new xml2js.Parser();
-            parser.parseString(data, (err, xml) => {
-                let result = parseTrack(xml.gpx.trk);
+const testStr = fs.readFileSync(`${__dirname}/test.gpx`);
 
-                result[0].should.be.instanceOf(TrackPoint);
-            });
-        });
+describe('parseTrack', () => {
+  it('should parse gpx track', () => {
+    let parser = new xml2js.Parser();
+    parser.parseString(testStr, (err, xml) => {
+      let result = parseTrack(xml.gpx.trk);
+      result[0].should.be.instanceOf(TrackPoint);
     });
+  });
 });
 
 describe('parse gpx', () => {
-    it('return an array', () => {
-        parseGpx(__dirname + '/test.gpx').then(data => {
-            data.should.be.Array();
-        });
-    });
+  it('return an array', async () => {
+    const data = await parseGpx(testStr);
+    data.should.be.Array();
+  });
 
-    it('array should contain TrackPoints', () => {
-        parseGpx(__dirname + '/test.gpx').then(data => {
-            data.forEach(t => {
-                t.should.be.instanceOf(TrackPoint);
-            });
-        });
-    });
+  it('array should contain TrackPoints', async () => {
+    const data = await parseGpx(testStr);
+    data.forEach(t => t.should.be.instanceOf(TrackPoint));
+  });
 
-    it('trackpoints should have all expected attributes', () => {
-        parseGpx(__dirname + '/test.gpx').then(data => {
-            let tp = data[0];
+  it('trackpoints should have longitude when attribute named lon', async () => {
+    const testLonStr = fs.readFileSync(`${__dirname}/test-lon.gpx`);
+    const data = await parseGpx(testLonStr);
+    let tp = data[0];
+    tp.should.have.property('longitude');
+  });
 
-            tp.should.have.property('latitude');
-            tp.should.have.property('longitude');
-            tp.should.have.property('timestamp');
-            tp.should.have.property('elevation');
-        });
-    });
+  it('should have heartrate if found', async () => {
+    const heartrateStr = fs.readFileSync(`${__dirname}/heartrate.gpx`);
+    const data = await parseGpx(heartrateStr);
+    let tp = data[0];
+    tp.heartrate.should.equal('114');
+  });
 
-    it('trackpoints should have longitude when attribute named lon', () => {
-        parseGpx(__dirname + '/test-lon.gpx').then(data => {
-            let tp = data[0];
-            tp.should.have.property('longitude');
-        });
-    });
+  it('should have cadence if found', async () => {
+    const cadenceStr = fs.readFileSync(`${__dirname}/cadence.gpx`);
+    const data = await parseGpx(cadenceStr);
+    let tp = data[0];
+    tp.cadence.should.equal('99');
+  });
 
-
-    it('should have heartrate if found', () => {
-        parseGpx(__dirname + '/heartrate.gpx').then(data => {
-            let tp = data[0];
-            tp.heartrate.should.equal('114');
-        });
-    });
-
-    it('should have cadence if found', () => {
-        parseGpx(__dirname + '/cadence.gpx').then(data => {
-            let tp = data[0];
-            tp.cadence.should.equal('99');
-        });
-    });
-
-
-    it('should return an error when unable to parse xml', () => {
-        parseGpx(__dirname + '/invalid.gpx').then(()=>{}, err => {
-            err.should.be.instanceOf(Error);
-        });
-    });
-
-    it('should return an error when given a non existant file', () => {
-        parseGpx('nonexistant.gpx').then(()=>{}, err => {
-            err.should.be.instanceOf(Error);
-            err.errno.should.be.equal(-2);
-        });
-    });
+  it('should return an error when unable to parse xml', async () => {
+    const invalidStr = fs.readFileSync(`${__dirname}/invalid.gpx`);
+    try {
+      await parseGpx(invalidStr);
+    } catch (err) {
+      err.should.be.instanceOf(Error);
+    };
+  });
 });
